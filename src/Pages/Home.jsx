2c71c454e1d214a30202308a6a7e852fc5f22afd
Daniel <danielbb2000@gmail.com>
2022-06-10 15:06:29 -0300

    finalizado NavBat
    Co-Authored-By: be3013 <57907227+be3013@users.noreply.github.com>

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,23 @@
 import { Navbar } from "../Components/Navbar";
+import Logo from '../Assets/MM.png'
+import '../Components/home.css';
 
 
 export const Home = () => {
   return (
     <>
     <Navbar />
-      <h1>Bem vindo ao microMercado</h1>
+      <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+      <table>
+        <tr>
+          <th>
+            <img src={Logo} className="logo" alt="Logo do site" />
+            <div>Por: 150 R$</div>
+          </th>
+          <th>a</th>
+          <th>a</th>
+        </tr>
+      </table>
     </>
   );
 }
