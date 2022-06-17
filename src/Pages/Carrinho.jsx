99ef0108a254d9d3364cf900e39d0fed15d1b659
Daniel <danielbb2000@gmail.com>
2022-06-17 16:56:28 -0300

    montando objeto para posterior uso na integração

--- src/Pages/Carrinho.jsx
+++ src/Pages/Carrinho.jsx
@@ -29,19 +29,33 @@ export const Carrinho = ({ props }) => {
     quantidade: 10,
     preco: 8
   }];
- 
+
   const { carrinhoData } = useCompra();
   console.log('dataaa', carrinhoData);
 
   let totalPrice = carrinhoData.map(c => {
     return +c.preco * c.quantidade;
   })
-  if(totalPrice.length> 0){
+  if (totalPrice.length > 0) {
     totalPrice = totalPrice.reduce(function (acumulador, valorAtual, index, array) {
       return acumulador + valorAtual;
     });
   }
- 
+  const handleCompra = () => {
+    let products = [];
+    for (const c of carrinhoData) {
+      let id = c.id;
+      let qtd = c.quantidade;
+      products.push({ productId: id, quantidade: qtd });
+    }
+
+    const obj = {
+      clientId: '84rf8rew4f',
+      produtos: products
+    };
+    console.log('obj', obj);
+  }
+
   return (
     <>
       <Navbar />
@@ -60,16 +74,17 @@ export const Carrinho = ({ props }) => {
           // if(e.length > 0){
 
           return (
-            <CartComponent image={e.image} produto={e.produto} quantidade={e.quantidade} preco={e.preco} />
+            <CartComponent image={e.image} produto={e.produto} quantidade={e.quantidade} preco={e.preco} key={index}/>
           )
 
 
 
-        }): <h3 style={{textAlign: 'center'}}>Nenhum elemento no carrinho</h3>}
+        }) : <h3 style={{ textAlign: 'center' }}>Nenhum elemento no carrinho</h3>}
       </div>
       <div style={{ textAlign: 'center', marginBottom: '3%' }}>
-        <p>Total: <strong>{carrinhoData.length> 0 ? `R$ ${totalPrice.toFixed(2).toString().replace('.', ',')}` : 'R$ 0,00'}  </strong></p>
-        <button style={{ width: "15%", height: '50px', borderRadius: '30px', cursor: 'pointer', border: 'none', backgroundColor: '#FDFD96', fontSize: '18px', fontWeight: 'bold' }}>Comprar</button>
+        <p>Total: <strong>{carrinhoData.length > 0 ? `R$ ${totalPrice.toFixed(2).toString().replace('.', ',')}` : 'R$ 0,00'}  </strong></p>
+        {carrinhoData.length > 0 && <button style={{ width: "15%", height: '50px', borderRadius: '30px', cursor: 'pointer', border: 'none', backgroundColor: '#FDFD96', fontSize: '18px', fontWeight: 'bold' }} onClick={handleCompra}>Comprar</button>}
+
       </div>
     </>
   )
