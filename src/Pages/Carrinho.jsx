16edb11d933896ac9de5bc3bb4766bf120c56be1
Daniel <danielbb2000@gmail.com>
2022-06-18 12:07:49 -0300

    Feito integração da compra

--- src/Pages/Carrinho.jsx
+++ src/Pages/Carrinho.jsx
@@ -2,6 +2,12 @@ import { Navbar } from "../Components/Navbar";
 import Login from '../Assets/MM.png'
 import { CartComponent } from "../Components/CartComponent";
 import { useCompra } from "../Context/Compra";
+import axios from 'axios';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoadingSpinner from '../Components/Loader';
+import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 export const Carrinho = ({ props }) => {
   const DUMMY_CART = [{
@@ -30,8 +36,10 @@ export const Carrinho = ({ props }) => {
     preco: 8
   }];
 
-  const { carrinhoData } = useCompra();
-  console.log('dataaa', carrinhoData);
+  const { carrinhoData, setCarrinhoData } = useCompra();
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
 
   let totalPrice = carrinhoData.map(c => {
     return +c.preco * c.quantidade;
@@ -46,14 +54,41 @@ export const Carrinho = ({ props }) => {
     for (const c of carrinhoData) {
       let id = c.id;
       let qtd = c.quantidade;
-      products.push({ productId: id, quantidade: qtd });
+      products.push({ produtoId: id, quantidade: qtd });
     }
 
+    const id = localStorage.getItem('id');
     const obj = {
-      clientId: '84rf8rew4f',
+      clientId: id,
       produtos: products
     };
+
     console.log('obj', obj);
+    setIsLoading(true);
+
+    axios.post('http://localhost:3003/carrinho', {
+      clientId: id,
+      produtos: products
+    })
+      .then(res => {
+        console.log('res', res.data);
+        setIsLoading(false);
+        toast.success("Compra realizada com sucesso", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+
+          progress: undefined,
+        });
+        navigate('/');
+        setCarrinhoData([]);
+      })
+      .catch(err => {
+        console.log('err', err.message);
+        setIsLoading(false);
+      })
+
   }
 
   return (
@@ -74,18 +109,20 @@ export const Carrinho = ({ props }) => {
           // if(e.length > 0){
 
           return (
-            <CartComponent image={e.image} produto={e.produto} quantidade={e.quantidade} preco={e.preco} key={index}/>
+            <CartComponent image={e.image} produto={e.produto} quantidade={e.quantidade} preco={e.preco} key={index} />
           )
 
 
 
         }) : <h3 style={{ textAlign: 'center' }}>Nenhum elemento no carrinho</h3>}
       </div>
-      <div style={{ textAlign: 'center', marginBottom: '3%' }}>
-        <p>Total: <strong>{carrinhoData.length > 0 ? `R$ ${totalPrice.toFixed(2).toString().replace('.', ',')}` : 'R$ 0,00'}  </strong></p>
-        {carrinhoData.length > 0 && <button style={{ width: "15%", height: '50px', borderRadius: '30px', cursor: 'pointer', border: 'none', backgroundColor: '#FDFD96', fontSize: '18px', fontWeight: 'bold' }} onClick={handleCompra}>Comprar</button>}
+      {isLoading ? <LoadingSpinner /> :
+        <div style={{ textAlign: 'center', marginBottom: '3%' }}>
+          <p>Total: <strong>{carrinhoData.length > 0 ? `R$ ${totalPrice.toFixed(2).toString().replace('.', ',')}` : 'R$ 0,00'}  </strong></p>
+          {carrinhoData.length > 0 && <button style={{ width: "15%", height: '50px', borderRadius: '30px', cursor: 'pointer', border: 'none', backgroundColor: '#FDFD96', fontSize: '18px', fontWeight: 'bold' }} onClick={handleCompra}>Comprar</button>}
 
-      </div>
+        </div>
+      }
     </>
   )
 }
